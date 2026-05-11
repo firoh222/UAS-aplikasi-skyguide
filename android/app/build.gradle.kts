207319d7289b1b8131180eb03de084b9db3289ed
musfiroh1407-cyber <musfiroh1407@gmail.com>
2026-05-11 22:39:42 +0700

    masukkan icon tampilan di HP

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.sky_giude"
+    namespace = "com.firoh.sky_giude"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.sky_giude"
+        applicationId = "com.firoh.sky_giude"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
